cf4debc1db8298e3eeedc16ab2ee6d1db0a918c7
EduAlfa <wichocuerda1@gmail.com>
2022-05-07 02:39:50 -0600

    Función recorrido filas

--- logica.py
+++ logica.py
@@ -1,12 +1,13 @@
 #importación de librerías a utilizar
 from operator import truediv
 import random
+#Creación de clase para generar el cuerpo del sudoku
 
-class cuerpoSu():
+class cuerpo():
     def __init__(self):
         self.cuerpoSudoku=[]
         self.punteo=0
-
+#Elaboración de la Función de matriz 9*9 del sudoku
     def creandoCuerpo(self):
         self.cuerpoSudoku=[
             [0,0,0,0,0,0,0,0,0],
@@ -19,7 +20,7 @@ class cuerpoSu():
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0]
         ]
-    
+#Función de para calcular la dificultad del sudoku
     def dificultad(self):
 
         bandera = False
@@ -33,4 +34,17 @@ class cuerpoSu():
             if condicion == True
                 bandera = True
 
+#Función que recorre cada elemento de las filas y las columnas de la matriz para validar el sudoku
+    def recorrido(self,cuerpoSu):
+        banderazo = True
+#Recorrido en las filas para ver si los números se repiten
+    for filas in cuerpoSu:
+        bandera = self.validanosRepite(filas)
+        if bandera == True
+            print("no se repiten los números",filas)
+        else:
+            print("Los datos se repiten",filas)
+            banderazo = False
+
+    return True
     
