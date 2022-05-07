06799b33e9dd77d80ea1387c9489026518158ee9
EduAlfa <wichocuerda1@gmail.com>
2022-05-07 02:16:01 -0600

    funcion cuerpo y dificultad

--- logica.py
+++ logica.py
@@ -6,3 +6,31 @@ class cuerpoSu():
     def __init__(self):
         self.cuerpoSudoku=[]
         self.punteo=0
+
+    def creandoCuerpo(self):
+        self.cuerpoSudoku=[
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]
+        ]
+    
+    def dificultad(self):
+
+        bandera = False
+        while bandera == False:
+            print("")
+            self.cuerpoSudoku[1][6] = random.randrange(1,9)
+            self.cuerpoSudoku[2][8] = random.randrange(1,9)
+            self.cuerpoSudoku[9][5] = random.randrange(1,9)
+            #Asignamos las posiciones en donde va a estar la numeración aleatoria
+            condicion = self.recorriendoCuerpo(self.cuerpoSudoku)
+            if condicion == True
+                bandera = True
+
+    
