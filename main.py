4d8ede7571e82d1c65ffb575a57d3c67a9bd4302
EduAlfa <wichocuerda1@gmail.com>
2022-05-07 01:46:09 -0600

    Corrección

--- main.py
+++
@@ -1,9 +0,0 @@
-#importación de librerías a utilizar
-from operator import truediv
-import random
-
-class cuerpoSu():
-    def __init__(self):
-        self.cuerpoSudoku=[]
-        self.punteo=0
-
